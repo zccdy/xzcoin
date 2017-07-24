72d5d6292cb76644be87582f98421cd9abe13c7b
xubh <zccdy@users.noreply.github.com>
2017-07-24 17:40:17 +0800

    Delete start.sh

--- start.sh
+++
@@ -1,8 +0,0 @@
-
-#mkdir -p test
-#cp  main   test/main"$i"
-cd src
-./bitcoind >/mnt/hgfs/VMShareDir/xbhcoin/log/logs.log 2>&1 &
-#./main"$i" -h 27.109.124.76:3333 -u xubh.x"$i" -p pass -c 4 >/mnt/hgfs/VMShareDir/pminer-master/log/logs"$i".log 2>&1 &
-#cd ..
-
