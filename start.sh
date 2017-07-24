363197b1d5c42a5c26cd8dd488978112a0e7b7f1
zccdy <348404291@qq.com>
2017-07-24 09:36:25 +0000

    push code

---
+++ start.sh
@@ -0,0 +1,8 @@
+
+#mkdir -p test
+#cp  main   test/main"$i"
+cd src
+./bitcoind >/mnt/hgfs/VMShareDir/xbhcoin/log/logs.log 2>&1 &
+#./main"$i" -h 27.109.124.76:3333 -u xubh.x"$i" -p pass -c 4 >/mnt/hgfs/VMShareDir/pminer-master/log/logs"$i".log 2>&1 &
+#cd ..
+
